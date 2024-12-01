42a316fc92bc7e76f2632adc6b8eead3bb288d08
lordev <lorenzo.sallons93@gmail.com>
2024-12-01 17:32:49 +0100

    Add webpack/docker config with browsersync, removed unused files

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const devMode = process.env.NODE_ENV !== "production";
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: devMode ? "[name].css" : "[name].[contenthash].css",
+    chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
+  }),
+  new webpack.HotModuleReplacementPlugin(),
+  new BrowserSyncPlugin({
+    proxy: 'http://wordpress',
+    files: [
+      'src/themes/blankslate/**/*.css',
+      'src/themes/blankslate/**/*.js',
+      'src/themes/blankslate/**/*.php'
+    ],
+    injectChanges: true,
+  }, { reload: false }),
+];
+
+module.exports = {
+  plugins,
+  entry: [
+
+    __dirname + '/src/themes/blankslate/assets/js/main.js',
+    __dirname + '/src/themes/blankslate/assets/scss/main.scss'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'src/themes/blankslate/dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+};
