f6b812218e9b76baff26c51fe9b7b42fb7508880
lordev <lorenzo.sallons93@gmail.com>
2024-12-18 23:20:57 +0100

    change folder structure new map partials

--- src/themes/blankslate/partials/slider/slider-section.php
+++ src/themes/blankslate/partials/slider/slider-section.php
@@ -5,12 +5,12 @@ $position = get_query_var('position');
 
 <div class="row gx-4 mt-6">
     <div class="col-4 position-relative">
-        <?php get_template_part('slider-buttons') ?>
+        <?php get_template_part('partials/slider/slider-buttons') ?>
     </div>
     <div class="col-8 position-relative" style="height: 614px;"> <!-- dynamic height insert ! -->
         <div class="slider overflow-hidden position-absolute">
             <div class="slider-inner d-flex gap-4 " id="<?php echo $parent_field ?>">
-                <?php get_template_part('slider') ?>
+                <?php get_template_part('partials/slider/slider') ?>
             </div>
         </div>
     </div>
--- src/themes/blankslate/partials/slider/slider.php
+++ src/themes/blankslate/partials/slider/slider.php
@@ -5,17 +5,20 @@ $child_field = get_query_var('child_field');
 $field = ['image', 'title', 'text'];
 $data = get_acf_group_field_children($parent_field, $child_field, $field);
 
+
 foreach ($data as $value) {
-    $image = $value['image'];
+    $image = !$value['image'] ? '<img src="https://placehold.co/450x450" class="card-img-top" alt="...">' : '<img class="custom-card-img" src="' . $value['image']['url'] . '" alt="' . $value['image']['alt'] . '" />';
     $title = $value['title'];
     $text = $value['text'];
 ?>
     <div class="custom-card <?php echo ($slider === SLIDER::SHOP) ? 'custom-card--lg' : (($slider === SLIDER::TESTIMONIALS) ? 'custom-card--sm' : ''); ?>">
-        <img src="<?php echo esc_url($image['url']); ?>" class="custom-card-img-top" alt="<?php echo esc_attr($image['alt']) ?>">
+
+        <?php echo wp_kses_post($image); ?>
         <div class="custom-card-body">
             <b class="custom-card-title"><?php echo wp_kses_post($title); ?></b>
             <p class="custom-card-text"><?php echo wp_kses_post($text); ?></p>
         </div>
     </div>
-  
+
+
 <?php } ?>
